8312eedec583421467be5757e11ddb927d5a0782
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-09-22 13:21:39 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        }
+}
+        for (int item : data) {
+    public static void main(String[] args) {
+        }
+    }
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+
+
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
