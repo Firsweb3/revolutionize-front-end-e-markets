10615622b4261ee26733197832a98675a7cc63d9
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-09-22 04:57:18 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,46 @@
+}
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+main();
+function generateRandomData() {
+    const data = generateRandomData();
+        console.log("Random Number: " + item);
+    return data;
+    });
+    data.forEach(item => {
+    }
+}
+
+    const data = [];
+
+    return data;
+
+    for (let i = 0; i < 10; i++) {
+}
+    });
+}
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+
+function generateRandomData() {
+    const data = [];
+    data.forEach(item => {
+
+    const data = generateRandomData();
+function main() {
+    }def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+
+import random
+    return data
+def generate_random_data():
+
+if __name__ == "__main__":
+    for item in data:
+        print(f"Random Number: {item}")
+    main()
